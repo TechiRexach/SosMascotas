eeae24a7604ea422d3f66bb75ce546fbda7781d2
Techi Rexach <techi90@gmail.com>
2021-04-18 13:07:51 +0200

    cloudinary bugs

--- client/src/components/alertForm/alertForm.jsx
+++ client/src/components/alertForm/alertForm.jsx
@@ -45,6 +45,25 @@ function AlertForm(props){
             [event.target.name]: event.target.value,
         })
     };
+
+    const uploadPhoto = (event) => {
+        event.preventDefault()
+        const token = localStorage.getItem(AUTH_TOKEN)
+        const config = {headers: {Authorization: `Bearer ${token}`}}
+
+        const photoData = new FormData();
+        photoData.append('photo', newPhoto.photo);
+        photoData.append('upload_preset', 'shromt1d')
+
+        axios.post('https://api.cloudinary.com/v1_1/techirexach/image/upload', photoData, {headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}}, config)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    }
     
 
     const createAlert = (event) => {
@@ -54,8 +73,8 @@ function AlertForm(props){
         const config = {headers: {Authorization: `Bearer ${token}`}}
 
         const formData = new FormData();
-        formData.append('photo', newPhoto.photo);
-        formData.append('upload_preset', 'shromt1d')
+        // formData.append('photo', newPhoto.photo);
+        // formData.append('upload_preset', 'shromt1d')
         formData.append('species', newAlert.species)
         formData.append('name', newAlert.name)
         formData.append('breed', newAlert.breed)
@@ -135,7 +154,7 @@ function AlertForm(props){
             </div>
             {errorMessage && <p className='alert alert-danger newAlert'>{errorMessage}</p>}
             {wellDone && <p className='alert alert-success'>{wellDone}</p>}
-            <button className='btn btn-light buttonCreateAlert' onClick={createAlert}>Enviar</button>
+            <button className='btn btn-light buttonCreateAlert' onClick={createAlert, uploadPhoto}>Enviar</button>
         </form>
         </div>
     );
