6122fb7cc88ea1ccf4c4339a9d67a01779304031
Techi Rexach <techi90@gmail.com>
2021-04-19 11:40:23 +0200

    another bug

--- client/src/App.js
+++ client/src/App.js
@@ -43,7 +43,7 @@ function App() {
 
         <PrivateRoute path='/password/:id' component={UserPassword}/> 
       
-        <PrivateRoute path='/addalert/:id' component={AlertForm} />
+        <PrivateRoute path='/addalert/' component={AlertForm} />
     
         <Route path='/veranimal/:id' component={AlertView} />
 
